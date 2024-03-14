f5c8d61dcf4a7b7f14b2c2d98cda2ef66616a222
John Gaitho <johngaitho05@gmail.com>
2024-03-14 22:05:38 +0300

    TypeVar

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -19,6 +19,8 @@ V = TypeVar('V')
 
 def safely_get_value(dct: Dict[K, V], key: K, default: Optional[V] = None) -> \
         Optional[V]:
+    """Returns the value of key if it exists in the dict else the
+    default value"""
     if key in dct:
         return dct[key]
     else:
