be9450c14113c1bd3fb57010db3b6114e1b60986
John Gaitho <johngaitho05@gmail.com>
2024-03-14 22:34:41 +0300

    pycodestyle compliance

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -16,7 +16,7 @@ zoom_2x = zoom_array(array)
 
 zoom_3x = zoom_array(array, 3.0)
 """
-from typing import Tuple, Any, List
+from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
