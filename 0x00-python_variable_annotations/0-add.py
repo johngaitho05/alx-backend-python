1b72a1347afc72d0793a2312f8fb0278680105e2
John Gaitho <johngaitho05@gmail.com>
2024-03-14 21:03:18 +0300

    pycodestyle compliance

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Function that sums up 2 floats"""
 
-def add(a:float, b:float) -> float:
+
+def add(a: float, b: float) -> float:
     """returns sum of a and b"""
     return a + b
