82c241aee90d8a8fe0a62a70d8dcc724b234020c
John Gaitho <johngaitho05@gmail.com>
2024-03-18 22:16:54 +0300

    current wait

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,10 +9,16 @@ wait_n should return the list of all the delays (float values).
 The list of the delays should be in ascending order without using
 sort() because of concurrency.
 """
-
+import asyncio
+import bisect
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int = 10) -> list[float]:
     """Calls wait_random multiple times and returns the results as a list"""
-    return sorted([await wait_random(max_delay) for _ in range(n)])
+    delays = []
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        bisect.insort(delays, delay)
+    return delays
