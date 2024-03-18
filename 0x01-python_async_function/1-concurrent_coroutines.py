4ded05be3589f5c3f227b864574d03d79244651a
John Gaitho <johngaitho05@gmail.com>
2024-03-18 22:22:38 +0300

    concurrent await

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,13 +11,14 @@ sort() because of concurrency.
 """
 import asyncio
 import bisect
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> list[float]:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """Calls wait_random multiple times and returns the results as a list"""
-    delays: list = []
+    delays: List = []
     tasks = [wait_random(max_delay) for _ in range(n)]
     for task in asyncio.as_completed(tasks):
         delay = await task
