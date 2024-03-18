0a4e4d69b911123f5409802eba4bd5a9c6b41089
John Gaitho <johngaitho05@gmail.com>
2024-03-18 22:09:05 +0300

    concurrent await

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -14,6 +14,6 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """Wait for a random time and return the waited time"""
-    delay = random.uniform(max_delay, max_delay)
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
