7cf32c9708d3d765cd2827f2830161c3c09cff6b
John Gaitho <johngaitho05@gmail.com>
2024-04-08 10:55:09 +0300

    test get_json

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Test cases for utils"""
 import unittest
+from unittest.mock import patch, Mock
 
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -27,3 +28,26 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test KeyError"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, nested_key)
+
+
+class TestGetJson(unittest.TestCase):
+    """Test get_json function"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Test get_json function"""
+        mock_get.return_value = Mock()
+        mock_get.return_value.json.return_value = test_payload
+
+        # Call the function
+        result = get_json(test_url)
+
+        # Assert that requests.get is called once with the correct URL
+        mock_get.assert_called_once_with(test_url)
+
+        # Assert that the output of get_json is equal to test_payload
+        self.assertEqual(result, test_payload)
